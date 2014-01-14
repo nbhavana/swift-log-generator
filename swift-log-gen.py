6737194d56f1b54eba0003e155168d1c6eb267fd
Bhavana Nawale <bknawale@gmail.com>
2014-01-14 12:28:07 -0800

    Used argparse parser for command line options and arguments

--- swift-log-gen.py
+++ swift-log-gen.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
-
+1
 #The program  create log entries for 'num_of_log_entries' number of lines.
 #User-Input : num_of_log_entries -> number of log entries to create,output file
 
-#This program creates a log for number of entries in an output file. The log has 22 fields seperated by space as follows:
+#This program creates a log for number of entries in an output file. The log has 22 
+#fields seperated by space as follows:
 
-#f1 f f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15 f16 f17 f18 f19 f20 f21 f22
+#f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15 f16 f17 f18 f19 f20 f21 f22
 
-#f1 = month, f2 = day, f3 = hour/min/sec, f4 = node, f5 = process, f6 = ip address 1, f7 = ip address 2, f8 = day/month/year/hour/mins/secs, f9 = method, f10 = url, f11 = HTTP/1.0, f12 = response code, f13 = referer, f14 = user agent, f15 = auth_token, f16 = bytes received, f17 = bytes sent, f18 = etag, f19 = transaction id, f20 = logged hdrs, f21 = time taken, f22 = source
+#f1 = month, f2 = day, f3 = hour/min/sec, f4 = node, f5 = process, f6 = ip address 1,
+# f7 = ip address 2, f8 = day/month/year/hour/mins/secs, f9 = method, f10 = url,
+# f11 = HTTP/1.0, f12 = response code, f13 = referer, f14 = user agent, f15 = auth_token,
+#f16 = bytes received, f17 = bytes sent, f18 = etag, f19 = transaction id, f20 = logged hdrs,
+#f21 = time taken, f22 = source
 
 
 
@@ -15,7 +20,7 @@ import os
 import random
 import os,binascii
 import sys
-
+import argparse
 
 
 month = ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
@@ -71,11 +76,11 @@ def create_log_entry(num_of_log_entries,filename):
 
 if __name__ == "__main__":
 
-    num_of_arguments = len(sys.argv)
-    if (num_of_arguments < 5):
-        print "Usage: %s -n <num_of_arguments> -o <output filename>" % sys.argv[0]
-    else:
-        num_of_log_entries =  int(sys.argv[2])
-        filename = sys.argv[4]
-        create_log_entry(num_of_log_entries,filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n",required = True,action = "store",dest = "num",help = "number of log entries",type=int)
+    parser.add_argument("-o",required = True,action = "store",dest = "fname",help = "name of output file")
+    result = parser.parse_args(sys.argv[1:])
+    num_of_log_entries = result.num
+    filename = result.fname
+    create_log_entry(num_of_log_entries,filename)
         
